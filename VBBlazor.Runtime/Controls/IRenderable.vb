daf2106e4d676007a790ffce8919f5d1c14283cd
Joseph Guenther <joseph@codingcool.ca>
2024-01-27 14:56:25 -0500

    Renderable

---
+++ VBBlazor.Runtime/Controls/IRenderable.vb
@@ -0,0 +1,6 @@
+﻿Namespace Controls
+    Public Interface IRenderable
+        ReadOnly Property DataContext As Object
+        Function GetContent() As XElement
+    End Interface
+End Namespace
---
+++ VBBlazor.Runtime/Controls/Layout.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.AspNetCore.Components
+Imports Microsoft.AspNetCore.Components.Rendering
+
+Namespace Controls
+    Public Class Layout
+        Inherits LayoutComponentBase
+        Implements IRenderable
+
+        Public Property DataContext As Object Implements IRenderable.DataContext
+
+        Public Overridable Function GetContent() As XElement Implements IRenderable.GetContent
+            Return <div/>
+        End Function
+
+        Protected Overrides Sub BuildRenderTree(builder As RenderTreeBuilder)
+            Dim renderer As New XmlRenderer(Me, Me)
+            builder.AddContent(0, renderer.Render)
+        End Sub
+    End Class
+End Namespace
