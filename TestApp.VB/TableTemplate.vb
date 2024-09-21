e22f7cef9d63d4f9d2d5e7bc64a70ffe45bb0241
Joseph Guenther <joseph@codingcool.ca>
2024-09-21 14:00:01 -0400

    Add basic loop and templating example

---
+++ TestApp.VB/TableTemplate.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.AspNetCore.Components
+Imports Microsoft.AspNetCore.Components.Rendering
+Imports VBBlazor.Runtime.Controls
+
+Public Class TableTemplate(Of TItem)
+    Inherits Control
+
+    Public Sub New()
+        DataContext = Me
+    End Sub
+
+    <Parameter>
+    Public Property TableHeader As RenderFragment
+
+    <Parameter>
+    Public Property RowTemplate As RenderFragment(Of TItem)
+
+    <Parameter>
+    Public Property Items As IEnumerable(Of TItem)
+
+    Public ReadOnly Property ActualItems As IEnumerable(Of Object)
+        Get
+            Return Items.Cast(Of Object)
+        End Get
+    End Property
+
+    Public ReadOnly Property ActualRowTemplate As RenderFragment(Of Object)
+        Get
+            Return Function(item As Object) 
+                Return Sub(builder As RenderTreeBuilder)
+                    builder.OpenElement(0, "tr")
+                    builder.AddContent(1, RowTemplate(item))
+                    builder.CloseElement()
+                End Sub
+            End Function
+        End Get
+    End Property
+
+    Public Overrides Function GetContent() As XElement
+        Return <table class="table" xmlns:local="VBBlazor.Runtime.Controls">
+                   <thead>
+                       <tr>
+                           <local:Fragment ChildContent="@TableHeader"/>
+                       </tr>
+                   </thead>
+                   <tbody>
+                        <local:Loop Items="@ActualItems" Template="@ActualRowTemplate"/>
+                   </tbody>
+               </table>
+    End Function
+End Class
---
+++ VBBlazor.Runtime/Controls/Loop.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.AspNetCore.Components
+Imports Microsoft.AspNetCore.Components.Rendering
+
+Namespace Controls
+    Public Class [Loop]
+        Inherits Control
+
+        Public Sub New()
+            DataContext = Me
+        End Sub
+
+        <Parameter>
+        Public Property Items As IEnumerable(Of Object)
+
+        <Parameter>
+        Public Property Template As RenderFragment(Of Object)
+
+        Protected Overrides Sub BuildRenderTree(builder As RenderTreeBuilder)
+            Dim index As Integer = 0
+            For Each item In Items
+                builder.AddContent(0, Template(item))
+                index += 1
+            Next
+        End Sub
+    End Class
+End NameSpace
